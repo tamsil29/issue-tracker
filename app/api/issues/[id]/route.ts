c7a7e17bb734ae3581ab93f9646db1921d65d5c6
blurry <tamsilansari29@gmail.com>
2023-11-18 14:12:10 +0530

    build api for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -15,7 +15,7 @@ export async function PATCH(
     where: { id: +params.id },
   });
   if (!issue)
-    return NextResponse.json({ error: "Inavlid issue" }, { status: 400 });
+    return NextResponse.json({ error: "Inavlid issue" }, { status: 404 });
 
   const updatedIssue = await prisma.issue.update({
     where: { id: +params.id },
@@ -25,5 +25,23 @@ export async function PATCH(
     },
   });
 
-  return NextResponse.json(updatedIssue)
+  return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: +params.id },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Inavlid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: +issue.id },
+  });
+
+  return NextResponse.json({});
 }
