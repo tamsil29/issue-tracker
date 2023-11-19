19291edce3aa14aa28f8a56e92abbb72021797f9
blurry <tamsilansari29@gmail.com>
2023-11-19 16:24:26 +0530

    sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,8 +22,14 @@ const IssuesPage = async ({
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns.map((c) => c.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
   return (
     <div>
