a052dc3e861e01ec95371de8cb952af2b7c4ac33
blurry <tamsilansari29@gmail.com>
2023-11-19 14:01:47 +0530

    enhanced api to assign issues and update status

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,20 @@
 import { z } from "zod";
+import { Status } from "@prisma/client";
 
 export const issueSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, 'Description is required'),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required").max(65535),
+});
+
+const statusValues = Object.values(Status) as [string, ...string[]]
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+    assignedToUserId: z.string().min(1, "Assigned to userId is required").max(255).optional().nullable(),
+    status: z.enum(statusValues).optional()
 });
