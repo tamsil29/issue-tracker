af39e960245339b5f5ba8ef100f6b605d96a13d1
blurry <tamsilansari29@gmail.com>
2023-11-22 22:31:24 +0530

    fixed skeleton loader colors

--- app/components/Skeleton.tsx
+++ app/components/Skeleton.tsx
@@ -1,4 +1,20 @@
-import Skeleton from "react-loading-skeleton";
+"use client";
+
+import SkeletonLoader, { SkeletonProps } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export default Skeleton
+import React from "react";
+import { useTheme } from "next-themes";
+
+const Skeleton = ({ ...props }: SkeletonProps) => {
+  const { theme } = useTheme();
+  return (
+    <SkeletonLoader
+      baseColor={theme === "dark" ? "#222222" : ""}
+      highlightColor={theme === "dark" ? "#333333" : ""}
+      {...props}
+    />
+  );
+};
+
+export default Skeleton;
