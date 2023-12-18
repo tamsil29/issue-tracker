7c33f1c08d85b9db267acde2a6cd1de6070f6e4a
blurry <tamsilansari29@gmail.com>
2023-12-18 21:18:12 +0530

    added size in error message

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -4,7 +4,7 @@ import { Text } from "@radix-ui/themes";
 const ErrorMessage = ({ message }: { message: string }) => {
   if (!message) return null;
   return (
-    <Text color="red" as="p">
+    <Text color="red" as="p" size={"2"}>
       {message}
     </Text>
   );
